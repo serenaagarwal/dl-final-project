e048c2f85829ddfc456a5754c7b9f66d47ec80b8
Serena Agarwal <serenaagarwal@Ultra-Instict-Shaggy.localdomain>
2025-04-19 14:39:00 -0400

    preprocess

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,15 @@
+import os
+import tifffile as tif
+import numpy as np
+
+
+def load_tif_data(folder_path):
+    image_files = sorted([f for f in os.listdir(folder_path) if f.endswith('.tif')])
+    images = [tif.imread(os.path.join(folder_path, f)) for f in image_files]
+    return images, image_files
+
+
+
+
+
 
