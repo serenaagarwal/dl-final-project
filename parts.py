a672016c3958b66b97faf7a6b49b61461e0e90ec
Serena Agarwal <serena_agarwal@brown.edu>
2025-04-27 22:54:11 -0400

    finished parts

--- parts.py
+++ parts.py
@@ -8,7 +8,7 @@ class DoubleConv(tf.keras.layers.Layer):
         if not mid_channels:
             mid_channels = out_channels
         self.double_conv = tf.keras.models.Sequential(
-            tf.keras.layers.Conv2D(in_channels, mid_channels, kernel_size=3, padding=1) #can adjust kernel and padding to our dataset
+            tf.keras.layers.Conv2D(in_channels, mid_channels, kernel_size=3, padding=1) #can adjust kernel and padding to our dataset - SA
             tf.keras.layers.BatchNormalization(mid_channels)
             tf.keras.activations.relu()
             tf.keras.layers.Conv2D(mid_channels, out_channels, kernel_size=3, padding=1)
@@ -32,7 +32,34 @@ class Downscale(tf.keras.layers.Layer):
             return self.maxpool_conv(x)
 
 class Upscale(tf.keras.layers.Layer):
-    def __init__(self, in_channels, out_channels): #I am making the default bilinear for upsampling but I don't totally understand it. I will leave it for now but may change.
+    def __init__(self, in_channels, out_channels): #I am making the default bilinear for upsampling because I think it's best for our data. Subject to change. -SA
         super().__init__()
-        self.up = 
+        self.up = tf.keras.layers.Upsampling2D((2, 2), interpolation='bilinear')
+        self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+    
+    def call(self, x1, x2): #x1 is the matching 2d matrix on the other side, which is being concatenated to x2. - SA
+        x1 = self.up(x1)
+        #compare sizes between x1 and x2:
+        diffX = x2.shape[2] - x1.size[2] #compare widths
+        diffY = x2.shape[1] - x2.size[1] #compare heights
+
+        #if x1 and x2 aren't the same, adds padding to x1 before concatenation. This part is pretty different from medium post - SA 
+        if diffY !=0 or diffX != 0:
+            x1 = tf.pad(x1, paddings = [
+                [0, 0],
+                [diffY // 2, diffY-diffY // 2]
+                [diffX // 2, diffX-diffX // 2]
+                [0, 0]
+            ])
         
+        x = tf.concat([x2, x1], axis=-1)
+        return self.conv(x)
+    
+    def OutConv(tf.keras.layers.Layer):
+        def __init__(self, in_channels, out_channels):
+            super().__init__()
+            self.conv = tf.keras.layers.Conv2D(in_channels, out_channels, kernel_size=1)
+
+        def call(self, x):
+            return self.conv(x)
+
