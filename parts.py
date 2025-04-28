7c3e9406b783e7516e5611af726d0d3f7a069289
Serena Agarwal <serena_agarwal@brown.edu>
2025-04-27 23:43:24 -0400

    u-net

--- parts.py
+++ parts.py
@@ -8,11 +8,11 @@ class DoubleConv(tf.keras.layers.Layer):
         if not mid_channels:
             mid_channels = out_channels
         self.double_conv = tf.keras.models.Sequential(
-            tf.keras.layers.Conv2D(in_channels, mid_channels, kernel_size=3, padding=1) #can adjust kernel and padding to our dataset - SA
-            tf.keras.layers.BatchNormalization(mid_channels)
-            tf.keras.activations.relu()
-            tf.keras.layers.Conv2D(mid_channels, out_channels, kernel_size=3, padding=1)
-            tf.keras.layers.BatchNormalization(out_channels)
+            tf.keras.layers.Conv2D(in_channels, mid_channels, kernel_size=3, padding=1), #can adjust kernel and padding to our dataset - SA
+            tf.keras.layers.BatchNormalization(mid_channels),
+            tf.keras.activations.relu(),
+            tf.keras.layers.Conv2D(mid_channels, out_channels, kernel_size=3, padding=1),
+            tf.keras.layers.BatchNormalization(out_channels),
             tf.keras.layers.activations.relu()
         )
 
@@ -24,12 +24,12 @@ class Downscale(tf.keras.layers.Layer):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.maxpool_conv = tf.keras.models.Sequential(
-            tf.keras.layers.MaxPool2D(2)
+            tf.keras.layers.MaxPool2D(2),
             DoubleConv(in_channels, out_channels)
         )
 
-        def call(self, x):
-            return self.maxpool_conv(x)
+    def call(self, x):
+        return self.maxpool_conv(x)
 
 class Upscale(tf.keras.layers.Layer):
     def __init__(self, in_channels, out_channels): #I am making the default bilinear for upsampling because I think it's best for our data. Subject to change. -SA
@@ -55,11 +55,11 @@ class Upscale(tf.keras.layers.Layer):
         x = tf.concat([x2, x1], axis=-1)
         return self.conv(x)
     
-    def OutConv(tf.keras.layers.Layer):
-        def __init__(self, in_channels, out_channels):
-            super().__init__()
-            self.conv = tf.keras.layers.Conv2D(in_channels, out_channels, kernel_size=1)
+class OutConv(tf.keras.layers.Layer):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = tf.keras.layers.Conv2D(in_channels, out_channels, kernel_size=1)
 
-        def call(self, x):
-            return self.conv(x)
+    def call(self, x):
+        return self.conv(x)
 
---
+++ u-net.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+import numpy as np
+from .parts import *
+
+class UNet(tf.keras.layers.Layer):
+    def __init__(self, num_channels=3, num_classes = 1):
+        super(UNet, self).__init__()
+        #definitely possible to reduce number of layers and simplify if computation is too expensive -SA
+        self.num_channels = num_channels
+        self.num_classes = num_classes
+
+        self.input_conv = DoubleConv(num_channels, 64)
+        self.downscale1 = Downscale(64, 128)
+        self.downscale2 = Downscale(128, 256)
+        self.downscale3 = Downscale(256, 512)
+        self.downscale4 = Downscale(512, 512)
+        self.upscale1 = Upscale(1024, 256)
+        self.upscale2 = Upscale(512, 128)
+        self.upscale3 = Upscale(256, 64)
+        self.upscale4 = Upscale(128, 64)
+        self.output_conv = OutConv(64, num_classes)
+
+    def call(self, x):
+        x1 = self.input_conv(x)
+        x2 = self.downscale1(x1)
+        x3 = self.downscale2(x2)
+        x4 = self.downscale3(x3)
+        x5 = self.downscale4(x4)
+        x = self.upscale1(x5, x4)
+        x = self.upscale2(x, x3)
+        x = self.upscale3(x, x2)
+        x = self.upscale4(x, x1)
+        logits = self.output_conv(x)
+        return logits
+
+
+
+
+
